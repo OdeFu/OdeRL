0de5baae26e38a3c4af8677025b3d66b3122d1d7
Olli Aalto <olli.aalto@mystes.fi>
2016-07-01 12:00:50 +0300

    Add and load SDL2 libraries.

--- src/app.d
+++ src/app.d
@@ -1,5 +1,11 @@
+import derelict.sdl2.sdl;
+import derelict.sdl2.image;
+import derelict.sdl2.ttf;
 import std.stdio;
 
-void main() {
-	writeln("Hello World.");
+void main()
+{
+    DerelictSDL2.load(SharedLibVersion(2, 0, 2));
+    DerelictSDL2Image.load();
+    DerelictSDL2ttf.load();
 }
